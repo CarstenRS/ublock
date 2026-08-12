cb2ac39502cba03f92e72c9d7ab0cb080c073b8c
CarstenRS <carsten@bioinf.uni-leipzig.de>
2026-08-12 19:29:53 +0200

    Update tor-redirect.js

--- tor-redirect.js
+++ tor-redirect.js
@@ -1,38 +1,4 @@
 /// tor-redirect.js
 (function() {
-  const redirects = {
-  // Search
-  'reddit.com':
-    'https://reddittorjg6rue252oqsxryoxengawnmo46qy4kyii5wtqnwfj4ooad.onion',
-
-  'startpage.com':
-    'http://startpagel6srwcjlue4zgq3zevrujfaow726kjytqbbjyrswwmjzcqd.onion',
-
-  'duckduckgo.com':
-    'https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion',
-
-  // Email
-  'proton.me':
-    'https://protonmailrmez3lotccipshtkleegetolb73fuirgj7r4o4vfu7ozyd.onion',
-
-  // News
-  'propublica.org':
-    'http://p53lf57qovyuvwsc6xnrppyply3vtqm7l6pcobkmyqsiofyeznfu5uqd.onion',
-
-  // Tor Project
-  'torproject.org':
-    'https://onion.torproject.org'
-  };
-
-  const host = location.hostname.toLowerCase().replace(/^www\./, '');
-  const target = redirects[host];
-
-  if (!target || location.hostname.endsWith('.onion')) return;
-
-  const url = new URL(target);
-  url.pathname = location.pathname;
-  url.search = location.search;
-  url.hash = location.hash;
-
-  location.replace(url.href);
+    console.log('TOR REDIRECT SCRIPT EXECUTED');
 })();
